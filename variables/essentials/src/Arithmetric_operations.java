52387c5703ed80cd3b697dbcc4a36f5e0de6beb1
Daniel Medina <paginamedina@gmail.com>
2025-08-06 09:53:43 -0500

    fixing files

---
+++ variables/essentials/src/Arithmetric_operations.java
@@ -0,0 +1,16 @@
+public class Arithmetric_operations {
+    public static void main(String[] args){
+        int num = 78;
+        int num2 = 5;
+
+        int sum = num + num2;
+        int less = num - num2;
+        int division = num / num2;
+        int module = num % num2;
+        // ... all that you alredy know
+
+
+
+        System.out.println(sum);
+    }
+}
